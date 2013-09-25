4dcac9d55906fa430c8db9e76778a89d8f10a4d7
Takeshi Hagikura <shiketa1999@gmail.com>
2013-09-25 10:05:36 +0900

    changed project name

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName = "myFirstPlayApp"
+  val appName = "event-scala"
   val appVersion = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
